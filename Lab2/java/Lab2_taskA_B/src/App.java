6e13c25b0a0c776c7f0c5be2711b3c33cd7ac998
vlad-avr <99638385+vlad-avr@users.noreply.github.com>
2023-09-25 15:42:28 +0300

    Lab2 Task B completed
    The heist is on!

--- Lab2/java/Lab2_taskA_B/src/App.java
+++ Lab2/java/Lab2_taskA_B/src/App.java
@@ -3,5 +3,7 @@ public class App {
     public static void main(String[] args) throws Exception {
         BeeSearch beeSearch = new BeeSearch(100);
         beeSearch.start_search();
+        Robbery robbery = new Robbery(30);
+        robbery.start_heist();
     }
 }
--- Lab2/java/Lab2_taskA_B/src/Robbery.java
+++ Lab2/java/Lab2_taskA_B/src/Robbery.java
@@ -6,6 +6,7 @@ import java.util.concurrent.SynchronousQueue;
 public class Robbery {
     private final Integer number_of_parts;
     private List<Part> parts_list;
+    private final Integer max_sleep_time = 500;
 
     //Rand Generator
     SecureRandom rnd = new SecureRandom();
@@ -22,6 +23,38 @@ public class Robbery {
         }
     }
 
+    public void start_heist() throws InterruptedException{
+        Robbers robbers = new Robbers();
+        Thread ivanov = new Thread(()->{
+            try{
+                robbers.steal();
+            }catch(InterruptedException exception){
+                System.out.println("Oops, looks like someone got caught!");
+            }
+        });
+        Thread petrov = new Thread(()->{
+            try{
+                robbers.pack();
+            }catch(InterruptedException exception){
+                System.out.println("Oops, looks like someone got caught!");
+            }
+        });
+        Thread nechiporuk = new Thread(()->{
+            try{
+                robbers.count();
+            }catch(InterruptedException exception){
+                System.out.println("Oops, looks like someone got caught!");
+            }
+        });
+        ivanov.start();
+        petrov.start();
+        nechiporuk.start();
+
+        ivanov.join();
+        petrov.join();
+        nechiporuk.join();
+    }
+
     private class Part{
         private final String name;
         private final Integer value;
@@ -32,8 +65,42 @@ public class Robbery {
     }
 
     private class Robbers{
-        private final SynchronousQueue<Part> petrov_ivanov_connect = new SynchronousQueue<>();
-        private final SynchronousQueue<Part> nechiporuk_petrov_connect = new SynchronousQueue<>();
+        private final SynchronousQueue<Part> pack_steal_connect = new SynchronousQueue<>();
+        private final SynchronousQueue<Part> count_pack_connect = new SynchronousQueue<>();
+
+        public void steal() throws InterruptedException{
+            int cur_part_id = 0;
+            while(cur_part_id < parts_list.size()){
+                System.out.println("Ivanov is stealing " + parts_list.get(cur_part_id).name);
+                pack_steal_connect.put(parts_list.get(cur_part_id));
+                cur_part_id++;
+                Thread.sleep(rnd.nextInt(max_sleep_time) + 10);
+            }
+        }
+
+        public void pack() throws InterruptedException{
+            int cur_part_id = 0;
+            while(cur_part_id < parts_list.size()){
+                Part part_stolen = pack_steal_connect.take();
+                System.out.println("Petrov is packing " + part_stolen.name);
+                count_pack_connect.put(part_stolen);
+                cur_part_id++;
+                Thread.sleep(rnd.nextInt(max_sleep_time) + 10);
+            }
+        }
+
+        public void count() throws InterruptedException{
+            int cur_part_id = 0;
+            int sum = 0;
+            while(cur_part_id < parts_list.size()){
+                Part part_packed = count_pack_connect.take();
+                System.out.println("Nechiporuk is counting " + part_packed.name + " whih is worth " + part_packed.value + " $ \n");
+                sum += part_packed.value;
+                cur_part_id++;
+                Thread.sleep(rnd.nextInt(max_sleep_time) + 10);
+            }
+            System.out.println("\n HEIST RESULTS: \nValue of stolen goodies : " + sum + " $ ");
+        }
 
     }
 }
