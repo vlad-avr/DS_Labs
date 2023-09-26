3af556ac934ceb920d4b0ee1319f3a7da34a5a76
vlad-avr <99638385+vlad-avr@users.noreply.github.com>
2023-09-26 22:53:56 +0300

    I hate Golang
    I fucking hate golang (yeah I pushed code with errors so what now)

--- Lab3/go/Smokers.go
+++ Lab3/go/Smokers.go
@@ -1,5 +1,41 @@
 package main
 
+import (
+	"math/rand"
+	"sync"
+)
+
+type Smoker struct {
+	item string
+}
+
+type Table struct {
+	table [2]string
+}
+
+func select_components() (int, int) {
+	first := rand.Intn(3)
+	second := rand.Intn(3)
+	for first == second {
+		second = rand.Intn(3)
+	}
+	return first, second
+}
+
+func smoke(wg *sync.WaitGroup, is_smoking chan bool, table *[]string) {
+	for {
+		is_smoking <- false
+	}
+}
+
+func start_session(smoker1 chan Smoker, smoker2 chan Smoker, smoker3 chan Smoker, table chan Table) {
+	for {
+		var wg sync.WaitGroup
+	}
+}
+
 func main() {
-	print("Hello")
+	var smoker1 Smoker
+	smoker1.item = "tobacco"
+	start_session()
 }
