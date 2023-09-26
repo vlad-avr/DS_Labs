5f955ce5a3a129058fd75dbb8d772cec19e7ea14
vlad-avr <99638385+vlad-avr@users.noreply.github.com>
2023-09-26 21:41:34 +0300

    Lab3 Task B completed
    Had to scrap previous idea and remake it from scratch

--- Lab3/java/lab3/src/App.java
+++ Lab3/java/lab3/src/App.java
@@ -6,8 +6,8 @@ public class App {
     public static void main(String[] args) throws Exception {
         // HoneyPotFeast feast = new HoneyPotFeast(50, 400);
         // feast.start_feast();
-       // System.out.println("null");
-        BarberShop shop = new BarberShop(300, 800);
+        //System.out.println("null");
+        BarberShop shop = new BarberShop(1000, 2000, 100);
         shop.work();
     }
 }
--- Lab3/java/lab3/src/TaskB/BarberShop.java
+++ Lab3/java/lab3/src/TaskB/BarberShop.java
@@ -1,18 +1,73 @@
 package TaskB;
 
 import java.security.SecureRandom;
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedDeque;
+import java.util.concurrent.Semaphore;
 
 public class BarberShop {
-    private Barber barber = new Barber();
     private SecureRandom rnd = new SecureRandom();
+    private Queue<String> queue = new ConcurrentLinkedDeque<>();
+    private Semaphore semaphore = new Semaphore(1);
     private final Integer max_client_span;
     private final Integer min_client_span;
-    public BarberShop(Integer max, Integer min){
+    private final Integer work_time;
+    public BarberShop(Integer max, Integer min, Integer work_time){
         max_client_span = max;
         min_client_span = min;
+        this.work_time = work_time;
     }
 
     public void work(){
+        Thread barber = new Thread(new Runnable() {
+            @Override
+            public void run(){
+                make_haircut();
+            }
+        });
+
+        Thread clients = new Thread(new Runnable() {
+            @Override
+            public void run(){
+                add_clients();
+            }
+        });
+
+        barber.start();
+        clients.start();
+        try{
+            barber.join();
+            clients.join();
+        }catch(InterruptedException exception){
+            exception.printStackTrace();
+        }
+    }
+
+    private void make_haircut(){
+        while(true){
+            while(queue.size() > 0){
+                try{
+                    semaphore.acquire();
+                    String client = queue.poll();
+                    semaphore.release();
+                    System.out.println("\n Barber is making a haircut for " + client + "\n");
+                    Thread.sleep(work_time);
+                    System.out.println("\n Barber has finished workind with " + client + "\n");
+                }catch(InterruptedException exception){
+                    System.out.println("\n Barber must not be interrupted during his work!\n");
+                }
+            }
+            synchronized(this){
+                try{
+                    wait();
+                }catch(InterruptedException exception){
+                    System.out.println("\n Barber was interrupted >:(\n");
+                }
+            }
+        }
+    }
+
+    private void add_clients(){
         int counter = 0;
         while(true){
             try{
@@ -21,12 +76,17 @@ public class BarberShop {
                 System.out.println("\n Cannot wait for new clients :D \n");
             }
             counter++;
-            Thread client = new Thread(new Client(barber, "Client " + counter));
-            client.start();
+            String client = "Client " + counter;
             try{
-                client.join();
+                semaphore.acquire();
+                System.out.println("\n" + client + " has entered the queue\n");
+                queue.add(client);
+                semaphore.release();
             }catch(InterruptedException exception){
-                System.out.println("\n Client was interrupted while joining the queue :( \n");
+                System.out.println("\n Client couldn`t join the queue :(");
+            }
+            synchronized(this){
+                notify();
             }
         }
     }
