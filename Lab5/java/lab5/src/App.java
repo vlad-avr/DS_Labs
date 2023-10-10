5c6765b877f0f82ebc89732672b5bc42635a62ee
vlad-avr <99638385+vlad-avr@users.noreply.github.com>
2023-10-10 17:21:12 +0300

    started lab5 TaskC

--- Lab5/java/lab5/src/App.java
+++ Lab5/java/lab5/src/App.java
@@ -3,8 +3,8 @@ import TaskB.LineManager;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        // RecruitManager manager = new RecruitManager(200, 4);
-        // manager.start();
+        RecruitManager manager = new RecruitManager(200, 4);
+        manager.start();
         LineManager lineManager = new LineManager(20);
         lineManager.start();
     }
