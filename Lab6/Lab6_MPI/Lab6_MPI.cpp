16747d26db0bbc81ce906ca5c0dd7a25dde60d83
vlad-avr <99638385+vlad-avr@users.noreply.github.com>
2023-10-21 14:44:22 +0300

    implemented simple multiplication for matrices + some getters setters

--- Lab6/Lab6_MPI/Lab6_MPI.cpp
+++ Lab6/Lab6_MPI/Lab6_MPI.cpp
@@ -7,9 +7,18 @@
 int main(int argc, char* argv[])
 {
 	srand(time(0));
-	Matrix* matrix = new Matrix(4, 5);
-	matrix->generate_matrix(50, -25, 2);
+	Matrix* matrix = new Matrix(2, 2);
+	Matrix* matrix1 = new Matrix(2, 2);
+	matrix1->generate_matrix(5, -2, 0);
+	matrix->generate_matrix(5, -2, 0);
+	std::cout << "Matrix A : \n";
 	matrix->print();
+	std::cout << "\nMatrix B : \n";
+	matrix1->print();
+	Matrix* C = Matrix::simple_multiplication(matrix, matrix1);
+	std::cout << "\n Matrix A*B : \n";
+	C->print();
+	delete matrix1;
 	delete matrix;
  //   std::cout << "Hello World!\n";
 	//int myid, numprocs, namelen;
--- Lab6/Lab6_MPI/Matrix.h
+++ Lab6/Lab6_MPI/Matrix.h
@@ -26,10 +26,26 @@ public:
 		delete matrix;
 	}
 
+	double get(int i, int j) {
+		if (i < 0 || i >= rows || j < 0 || j >= cols) {
+			std::cout << "\nIndex out of bounds!\n";
+			return NULL;
+		}
+		return matrix[i][j];
+	}
+
+	double set(double val, int i, int j) {
+		if (i < 0 || i >= rows || j < 0 || j >= cols) {
+			std::cout << "\nIndex out of bounds!\n";
+			return NULL;
+		}
+		matrix[i][j] = val;
+	}
+
 	void generate_matrix(int max_val, int offset, int precision) {
 		for (int i = 0; i < rows; i++) {
 			for (int j = 0; j < cols; j++) {
-				matrix[i][j] = (rand() % max_val + offset) / (double)(10^precision);
+				matrix[i][j] = (rand() % max_val + offset) / (double)(pow(10, precision));
 			}
 		}
 	}
@@ -43,6 +59,29 @@ public:
 		}
 	}
 
+	int get_rows() {
+		return this->rows;
+	}
 
+	int get_cols() {
+		return this->cols;
+	}
+
+	static Matrix* simple_multiplication(Matrix* A, Matrix* B) {
+		if (A->cols != B->rows) {
+			std::cout << "\nInvalid matrices sizes (number of rows of A != number of columns of B)!\n";
+			return NULL;
+		}
+		Matrix* C = new Matrix(A->rows, B->cols);
+		for (int i = 0; i < A->rows; i++) {
+			for (int j = 0; j < B->cols; j++) {
+				C->matrix[i][j] = 0;
+				for (int k = 0; k < A->cols; k++) {
+					C->matrix[i][j] += A->matrix[i][k] * B->matrix[k][j];
+				}
+			}
+		}
+		return C;
+	}
 };
 
