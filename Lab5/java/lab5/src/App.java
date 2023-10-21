e4e07f2d135f354d9a69d0c3b0ecd1fcb4f2d126
vlad-avr <99638385+vlad-avr@users.noreply.github.com>
2023-10-21 12:57:31 +0300

    changing it up for c++/modified gitignore

--- Lab5/java/lab5/src/App.java
+++ Lab5/java/lab5/src/App.java
@@ -3,9 +3,9 @@ import TaskB.LineManager;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        RecruitManager manager = new RecruitManager(200, 4);
-        manager.start();
-        //LineManager lineManager = new LineManager(20);
-        // lineManager.start();
+        // RecruitManager manager = new RecruitManager(200, 4);
+        // manager.start();
+        LineManager lineManager = new LineManager(20);
+        lineManager.start();
     }
 }
--- Lab6/lab6/src/App.java
+++
@@ -1,8 +0,0 @@
-import tester.MultiplicationComparison;
-
-public class App {
-    public static void main(String[] args) throws Exception {
-        MultiplicationComparison comparison = new MultiplicationComparison();
-        comparison.start();
-    }
-}
