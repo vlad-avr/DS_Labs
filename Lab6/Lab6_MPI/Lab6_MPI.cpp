e35bf12af3fbe25b2b5cc9789ff0e3ad2951ab5f
vlad-avr <99638385+vlad-avr@users.noreply.github.com>
2023-10-24 18:20:38 +0300

    added basic functionality

--- Lab6/Lab6_MPI/Lab6_MPI.cpp
+++ Lab6/Lab6_MPI/Lab6_MPI.cpp
@@ -2,30 +2,27 @@
 #include <mpi.h>
 #include <random>
 #include <time.h>
+#include "Matrix.h"
 
 int main(int argc, char* argv[])
 {
 	srand(time(0));
-	/*Matrix* matrix = new Matrix(2, 2);
-	Matrix* matrix1 = new Matrix(2, 2);
-	matrix1->generate_matrix(5, -2, 0);
-	matrix->generate_matrix(5, -2, 0);
-	std::cout << "Matrix A : \n";
-	matrix->print();
-	std::cout << "\nMatrix B : \n";
-	matrix1->print();
-	Matrix* C = Matrix::simple_multiplication(matrix, matrix1);
-	std::cout << "\n Matrix A*B : \n";
-	C->print();
-	delete matrix1;
-	delete matrix;*/
-    MPI_Init(&argc, &argv);
+	double* A = Matrix::generateRandom(20, 5, 10, 4);
+	double* B = Matrix::generateFilled(2, 4);
+	std::cout << "\n A:\n";
+	Matrix::print(A, 4);
+	std::cout << "\n B:\n";
+	Matrix::print(B, 4);
+	double* C = Matrix::simpleMultiplication(A, B, 4);
+	std::cout << "\n C:\n";
+	Matrix::print(C, 4);
+    /*MPI_Init(&argc, &argv);
 
     int rank, size;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 	std::cout << "\nPrinting from task " << rank << " \\ " << size;
-	MPI_Finalize();
+	MPI_Finalize();*/
 	return 0;
 }
 
--- Lab6/Lab6_MPI/Matrix.h
+++ Lab6/Lab6_MPI/Matrix.h
@@ -26,10 +26,25 @@ public:
 
 	static double* simpleMultiplication(double* A, double* B, unsigned int size) {
 		unsigned int actual_size = size * size;
-		
+		double* C = new double[actual_size];
+		for (int i = 0; i < size; i++) {
+			for (int j = 0; j < size; j++) {
+				C[i * size + j] = 0.0;
+				for (int k = 0; k < size; k++) {
+					C[i * size + j] += A[i * size + k] * B[k * size + j];
+				}
+			}
+		}
+		return C;
 	}
 
-private:
-
+	static void print(double* A, unsigned int size){
+		for (int i = 0; i < size; i++) {
+			for (int j = 0; j < size; j++) {
+				std::cout << A[i * size + j] << " ";
+			}
+			std::cout << std::endl;
+		}
+	}
 };
 
