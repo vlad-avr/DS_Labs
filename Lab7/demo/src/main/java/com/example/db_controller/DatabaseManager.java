022c7b1382bf94e31d84f061d559cc4256cf4e95
vlad-avr <99638385+vlad-avr@users.noreply.github.com>
2023-10-31 20:07:35 +0200

    Added DatabaseManager class

--- Lab7/demo/src/main/java/com/example/db_controller/DatabaseManager.java
+++ Lab7/demo/src/main/java/com/example/db_controller/DatabaseManager.java
@@ -1,5 +1,22 @@
 package com.example.db_controller;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class DatabaseManager {
-    
+    private final String db_url = "jdbc:sqlite:library.db";
+
+    private Connection connect() {
+        Connection con = null;
+        try {
+            con = DriverManager.getConnection(db_url);
+        } catch (SQLException exception) {
+            System.out.println(exception.getMessage());
+        }
+        return con;
+    }
 }
