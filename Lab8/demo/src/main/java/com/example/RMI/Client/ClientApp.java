52f8822fb74f7041fd608d5bb1656ed1282b54dd
vlad-avr <99638385+vlad-avr@users.noreply.github.com>
2023-11-18 17:20:25 +0200

    Started working on RMI implementation

---
+++ Lab8/demo/src/main/java/com/example/RMI/Client/ClientApp.java
@@ -0,0 +1,8 @@
+package com.example.RMI.Client;
+
+public class ClientApp {
+    
+    public static void main(String[] args){
+        
+    }
+}
---
+++ Lab8/demo/src/main/java/com/example/RMI/Server/ServerApp.java
@@ -0,0 +1,8 @@
+package com.example.RMI.Server;
+
+public class ServerApp {
+    
+    public static void main(String[] args){
+        
+    }
+}
