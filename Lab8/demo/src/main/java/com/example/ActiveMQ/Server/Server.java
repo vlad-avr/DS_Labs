05ea8eae6468a95ad166428a7caeefe008caedea
vlad-avr <99638385+vlad-avr@users.noreply.github.com>
2023-11-20 16:58:58 +0200

    connected activeMQ and started to learn about this mess

---
+++ Lab8/demo/src/main/java/com/example/ActiveMQ/Server/Server.java
@@ -0,0 +1,42 @@
+package com.example.ActiveMQ.Server;
+
+import javax.jms.*;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+
+public class Server {
+    public Server(){
+        try {
+            // Create a connection factory
+            ConnectionFactory connectionFactory = new ActiveMQConnectionFactory("tcp://localhost:61616");
+
+            // Create a connection
+            Connection connection = connectionFactory.createConnection();
+            connection.start();
+
+            // Create a session
+            Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+
+            // Create a topic
+            Topic topic = session.createTopic("myTopic");
+
+            // Create a producer
+            MessageProducer producer = session.createProducer(topic);
+
+            // Create a message
+            TextMessage message = session.createTextMessage("Hello, Clients!");
+
+            // Send the message to the topic
+            producer.send(message);
+
+            System.out.println("Message sent to the topic");
+
+            // Close resources
+            producer.close();
+            session.close();
+            connection.close();
+        } catch (JMSException e) {
+            e.printStackTrace();
+        }
+    }
+}
