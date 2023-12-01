0a4b5ba8b12fa62486258a7c95670f6d0fe6c4e1
vlad-avr <99638385+vlad-avr@users.noreply.github.com>
2023-12-01 18:18:42 +0200

    I FUCKING HATE AUTOGRADER

--- Courses/java_course/parallel_programming/week 4 (again)/miniproject_4/src/main/java/edu/coursera/parallel/OneDimAveragingPhaser.java
+++ Courses/java_course/parallel_programming/week 4 (again)/miniproject_4/src/main/java/edu/coursera/parallel/OneDimAveragingPhaser.java
@@ -63,12 +63,10 @@ public final class OneDimAveragingPhaser {
                 double[] threadPrivateMyVal = myVal;
                 double[] threadPrivateMyNew = myNew;
 
-                final int chunkSize = (n + tasks - 1) / tasks;
-                final int left = (i * chunkSize) + 1;
-                int right = (left + chunkSize) - 1;
-                if (right > n) right = n;
-
                 for (int iter = 0; iter < iterations; iter++) {
+                    final int left = i * (n / tasks) + 1;
+                    final int right = (i + 1) * (n / tasks);
+
                     for (int j = left; j <= right; j++) {
                         threadPrivateMyNew[j] = (threadPrivateMyVal[j - 1]
                             + threadPrivateMyVal[j + 1]) / 2.0;
@@ -97,7 +95,7 @@ public final class OneDimAveragingPhaser {
      * uses the Phaser.arrive and Phaser.awaitAdvance APIs to overlap
      * computation with barrier completion.
      *
-     * DONE Complete this method based on the provided runSequential and
+     * TODO Complete this method based on the provided runSequential and
      * runParallelBarrier methods.
      *
      * @param iterations The number of iterations to run
@@ -108,55 +106,55 @@ public final class OneDimAveragingPhaser {
      * @param tasks The number of threads/tasks to use to compute the solution
      */
     public static void runParallelFuzzyBarrier(final int iterations,
-                                               final double[] myNew, final double[] myVal, final int n,
-                                               final int tasks) {
-        Phaser[] phaserArray = new Phaser[tasks];
-        for(int i=0;i<phaserArray.length;i++){
-            phaserArray[i] = new Phaser(1);
-        }
-
-        Thread[] threads = new Thread[tasks];
-
-        for (int ii = 0; ii < tasks; ii++) {
-            final int i = ii;
-
-            threads[ii] = new Thread(() -> {
-                double[] threadPrivateMyVal = myVal;
-                double[] threadPrivateMyNew = myNew;
-
-                for (int iter = 0; iter < iterations; iter++) {
-                    final int left = i * (n / tasks) + 1;
-                    final int right = (i + 1) * (n / tasks);
-
-                    for (int j = left; j <= right; j++) {
-                        threadPrivateMyNew[j] = (threadPrivateMyVal[j - 1]
-                                + threadPrivateMyVal[j + 1]) / 2.0;
-                    }
-                    System.out.println("Arriving task: "+ i);
-                    phaserArray[i].arrive();
-                    if(i-1>=0){
-                        System.out.println("Arrived task "+ i +" Waiting for "+ (i-1));
-                        phaserArray[i-1].awaitAdvance(iter);
-                    }
-                    if(i+1<tasks){
-                        System.out.println("Arrived task "+ i +" Waiting for "+ (i+1));
-                        phaserArray[i+1].awaitAdvance(iter);
-                    }
-
-                    double[] temp = threadPrivateMyNew;
-                    threadPrivateMyNew = threadPrivateMyVal;
-                    threadPrivateMyVal = temp;
+            final double[] myNew, final double[] myVal, final int n,
+            final int tasks) {
+                Phaser[] phs = new Phaser[tasks];
+                for(int i=0;i<phs.length;i++){
+                    phs[i] = new Phaser(1);
                 }
-            });
-            threads[ii].start();
-        }
-
-        for (int ii = 0; ii < tasks; ii++) {
-            try {
-                threads[ii].join();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+        
+                Thread[] threads = new Thread[tasks];
+        
+                for (int ii = 0; ii < tasks; ii++) {
+                    final int i = ii;
+        
+                    threads[ii] = new Thread(() -> {
+                        double[] threadPrivateMyVal = myVal;
+                        double[] threadPrivateMyNew = myNew;
+        
+                        for (int iter = 0; iter < iterations; iter++) {
+                            final int left = i * (n / tasks) + 1;
+                            final int right = (i + 1) * (n / tasks);
+        
+                            for (int j = left; j <= right; j++) {
+                                threadPrivateMyNew[j] = (threadPrivateMyVal[j - 1]
+                                        + threadPrivateMyVal[j + 1]) / 2.0;
+                            }
+        //                    System.out.println("Arriving task: "+ i);
+                            phs[i].arrive();
+                            if(i-1>=0){
+        //                        System.out.println("Arrived task "+ i +" Waiting for "+ (i-1));
+                                phs[i-1].awaitAdvance(iter);
+                            }
+                            if(i+1<tasks){
+        //                        System.out.println("Arrived task "+ i +" Waiting for "+ (i+1));
+                                phs[i+1].awaitAdvance(iter);
+                            }
+        
+                            double[] temp = threadPrivateMyNew;
+                            threadPrivateMyNew = threadPrivateMyVal;
+                            threadPrivateMyVal = temp;
+                        }
+                    });
+                    threads[ii].start();
+                }
+        
+                for (int ii = 0; ii < tasks; ii++) {
+                    try {
+                        threads[ii].join();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
             }
         }
-    }
-}
