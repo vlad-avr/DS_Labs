08635295827bad2cdbe3865c4dd7e04f70629f0f
vlad-avr <99638385+vlad-avr@users.noreply.github.com>
2023-12-01 18:49:09 +0200

    updted week 4 task

--- Courses/java_course/distributed_programming/miniproject_4/src/main/java/edu/coursera/distributed/FileServer.java
+++ Courses/java_course/distributed_programming/miniproject_4/src/main/java/edu/coursera/distributed/FileServer.java
@@ -1,8 +1,15 @@
 package edu.coursera.distributed;
 
-import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.io.BufferedReader;
+import java.io.File;
 
 /**
  * A basic and very limited implementation of a file server that responds to GET
@@ -13,8 +20,8 @@ public final class FileServer {
      * Main entrypoint for the basic file server.
      *
      * @param socket Provided socket to accept connections on.
-     * @param fs A proxy filesystem to serve files from. See the PCDPFilesystem
-     *           class for more detailed documentation of its usage.
+     * @param fs     A proxy filesystem to serve files from. See the PCDPFilesystem
+     *               class for more detailed documentation of its usage.
      * @param ncores The number of cores that are available to your
      *               multi-threaded file server. Using this argument is entirely
      *               optional. You are free to use this information to change
@@ -23,46 +30,46 @@ public final class FileServer {
      *                     should be a fatal error, your file server
      *                     implementation is not expected to ever throw
      *                     IOExceptions during normal operation.
-     *
-             */
+     */
     public void run(final ServerSocket socket, final PCDPFilesystem fs,
-                    final int ncores) throws IOException {
+            final int ncores) throws IOException {
         /*
          * Enter a spin loop for handling client requests to the provided
          * ServerSocket object.
          */
         while (true) {
-            //Use socket.accept to get a Socket object
-
-            Socket s = socket.accept();
+            Socket socket_ = socket.accept();
 
             Thread thread = new Thread(
                     () -> {
                         try {
-                            BufferedReader reader = new BufferedReader(new InputStreamReader(s.getInputStream()));
-                            String line = reader.readLine();
+                            InputStream inputStream = socket_.getInputStream();
+                            InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+                            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+                            String line = bufferedReader.readLine();
                             assert line != null;
                             assert line.startsWith("GET");
-                            PCDPPath path = new PCDPPath(line.split(" ")[1]);
-                            PrintWriter writer = new PrintWriter(s.getOutputStream());
-                            String fileContent;
-                            if ((fileContent = fs.readFile(path)) != null) {
-                                writer.write("HTTP/1.0 200 OK\r\n");
-                                writer.write("Server: FileServer\r\n");
-                                writer.write("\r\n");
-                                writer.write(fileContent);
-                            } else {
-                                writer.write("HTTP/1.0 404 Not Found\r\n");
-                                writer.write("Server: FileServer\r\n");
-                                writer.write("\r\n");
-                            }
-                            writer.close();
-                            // Flush out and close the stream
-                        } catch (IOException e) {
 
+                            PCDPPath path = new PCDPPath(line.split(" ")[1]);
+                            String content = fs.readFile(path);
+
+                            OutputStream outputStream = socket_.getOutputStream();
+                            PrintWriter writer = new PrintWriter(outputStream);
+
+                            if (content != null) {
+                                writer.write("HTTP/1.0 200 OK\r\nServer: FileServer\r\n\r\n");
+                                writer.write(content + "\r\n");
+                            } else writer.write("HTTP/1.0 404 Not Found\r\nServer: FileServer\r\n\r\n");
+                            writer.close();
+
+                        } catch (IOException e) {
+                            e.printStackTrace();
                         }
                     }
-            );
+                    
+                );
+
             thread.start();
         }
     }
